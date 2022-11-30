b8cf3b86f749d5e2bdb30d9da54d3adae7844d51
AtleNyhus <anyhus19@student.aau.dk>
2022-11-30 12:48:42 +0100

    Should be done?

--- Assets/LineLogicScripts/Interupt.cs
+++ Assets/LineLogicScripts/Interupt.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class Interupt : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private float timer = 0;
+    private float timerEnd = 5;
+    private void Awake()
     {
-        
+        Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Initialize()
     {
-        
+        gameObject.GetComponentInChildren<TextMeshPro>().text =
+            "!! " + gameObject.GetComponentInChildren<TextMeshPro>().text + " !!";
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= timerEnd) Destroy(this);
     }
 }
